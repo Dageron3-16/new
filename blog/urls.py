bf8ca60898d5c684e1cf6c4fc46dbee8159f628f
Dageron316 <98896201+Dageron316@users.noreply.github.com>
2022-04-12 15:45:17 -0400

    progreso

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django import views
+from django.urls import path
+from .views import BlogListView
+
+
+app_name="blog"
+
+urlpatterns = [
+   
+    path('', BlogListView.as_view(), name="home" )
+]
